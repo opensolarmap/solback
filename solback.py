42c7a4ad9bf8ee0e319e19ce3b3f13b2f3d71d13
cquest <cquest@openstreetmap.fr>
2015-11-07 11:15:18 +0100

    hello solar world !

---
+++ solback.py
@@ -0,0 +1,30 @@
+# solback.py
+
+# Let's get this party started
+import falcon
+import psycopg2
+
+class BuildingsResource(object):
+    def on_get(self, req, resp):
+        db = psycopg2.connect("dbname=osm user=cquest")
+        cur = db.cursor()
+        cur.execute("""SELECT format('{"id":%s,"lat":%s,"lon":%s,"surface":%s,"geom":%s}',
+            osm_id, round(st_y(st_centroid(geom))::numeric,6), round(st_x(st_centroid(geom))::numeric,6),
+            round(surface::numeric), st_asgeojson(geom)) FROM buildings limit 1;"""
+        )
+        building = cur.fetchone()
+
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = (building[0])
+
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+# Resources are represented by long-lived class instances
+buildings = BuildingsResource()
+
+# things will handle all requests to the '/things' URL path
+app.add_route('/building', buildings)
+
