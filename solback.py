6a5eb4349e813688f4bf255cf14eead6b59b4098
cquest <cquest@openstreetmap.fr>
2015-11-07 11:50:34 +0100

    a real geojson is better !

--- solback.py
+++ solback.py
@@ -8,9 +8,9 @@ class BuildingsResource(object):
     def on_get(self, req, resp):
         db = psycopg2.connect("dbname=osm user=cquest")
         cur = db.cursor()
-        cur.execute("""SELECT format('{"id":%s,"lat":%s,"lon":%s,"surface":%s,"geom":%s}',
+        cur.execute("""SELECT format('{"type":"Feature","properties":{"id":%s,"lat":%s,"lon":%s,"surface":%s},"geometry":%s}',
             osm_id, round(st_y(st_centroid(geom))::numeric,6), round(st_x(st_centroid(geom))::numeric,6),
-            round(surface::numeric), st_asgeojson(geom)) FROM buildings limit 1;"""
+            round(surface::numeric), st_asgeojson(geom,6)) FROM buildings limit 1;"""
         )
         building = cur.fetchone()
 
