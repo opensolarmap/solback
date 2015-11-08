cf01d5fa57c6a72f2cfe62edd0427d9379ebf021
cquest <cquest@openstreetmap.fr>
2015-11-08 12:34:35 +0100

    stats added

--- solback.py
+++ solback.py
@@ -10,6 +10,7 @@ class BuildingsResource(object):
         ip = req.env['REMOTE_ADDR']
         db = psycopg2.connect("dbname=osm user=cquest")
         cur = db.cursor()
+        # get one random building around our location
         cur.execute("""SELECT '{"type":"Feature","properties":{"id":'|| osm_id::text
             ||',"lat":'|| round(st_y(st_centroid(geom))::numeric,6)::text
             ||',"lon":'|| round(st_x(st_centroid(geom))::numeric,6)::text
@@ -47,13 +48,45 @@ class BuildingsResource(object):
         db.commit()
         cur.close()
         db.close()
+        resp.set_header('X-Powered-By', 'OpenSolarMap')
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Headers', 'X-Requested-With')
+        resp.status = falcon.HTTP_200
 
+class StatsResource(object):
+    def on_get(self, req, resp):
+        ip = req.env['REMOTE_ADDR']
+        db = psycopg2.connect("dbname=osm user=cquest")
+        cur = db.cursor()
+        cur.execute("SELECT count(*) from building_orient where ip = '%s';" % ip)
+        stat = cur.fetchone()
+        count_ip = stat[0]
+        cur.execute("SELECT count(*) from building_orient;")
+        stat = cur.fetchone()
+        count_total = stat[0]
+        cur.execute("SELECT count(distinct(ip)) from building_orient;")
+        stat = cur.fetchone()
+        count_ips = stat[0]
+        cur.execute("SELECT count(distinct(id)) from building_orient;")
+        stat = cur.fetchone()
+        count_buildings = stat[0]
+        cur.close()
+        db.close()
+
+        resp.body = """{"count_total":%s,"count_ip":%s,"count_ips":%s,"count_buildings":%s}""" % (count_total, count_ip, count_ips, count_buildings)
+        resp.set_header('X-Powered-By', 'OpenSolarMap')
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Headers', 'X-Requested-With')
+        resp.status = falcon.HTTP_200
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
 
 # Resources are represented by long-lived class instances
 buildings = BuildingsResource()
+stats = StatsResource()
 
-# things will handle all requests to the '/things' URL path
+# things will handle all requests to the matching URL path
 app.add_route('/building', buildings)
+app.add_route('/stats', stats)
+
