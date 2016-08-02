cebdcfe18ff3aef67dd4d3aacd70c56f911a4775
cquest <cquest@openstreetmap.fr>
2016-08-02 19:41:32 +0200

    request to get several buildings at once

--- solback.py
+++ solback.py
@@ -3,32 +3,40 @@
 import falcon
 import psycopg2
 import uuid
-
+import json
 class BuildingsResource(object):
     def getBuilding(self, req, resp):
         db = psycopg2.connect("dbname=opensolarmap user=cquest")
         cur = db.cursor()
         ip = req.access_route[0]
+
+        # request a list of buildings, not just one...
+        if 'limit' in req.params:
+            limit = int(req.params['limit'])
+            if limit > 100:
+                limit = 100
+        else:
+            limit = 1
         # get a building from the allready partially crowdsourced ones
-        query = """SELECT '{"type":"Feature","properties":{"id":'|| osm_id::text
+        query = cur.mogrify("""SELECT '{"type":"Feature","properties":{"id":'|| osm_id::text
             ||',"lat":'|| round(st_y(st_centroid(geom))::numeric,6)::text
             ||',"lon":'|| round(st_x(st_centroid(geom))::numeric,6)::text
             ||',"surface":'|| round(surface::numeric)::text
             ||',"radius":'|| round(st_length(st_longestline(geom,geom)::geography)::numeric/2,1)::text
             ||'},"geometry":'|| st_asgeojson(geom,6) ||'}'
             FROM building_next n
-            LEFT JOIN building_orient o ON (o.id=n.id and o.ip='%s')
+            LEFT JOIN building_orient o ON (o.id=n.id and o.ip=%s)
             JOIN buildings b ON (b.osm_id=n.id)
             WHERE n.total<10 AND o.ip is null
             GROUP BY b.osm_id, b.geom, b.surface, n.nb, n.last, b.orient_type
             HAVING orient_type is null
-            ORDER BY n.nb desc, n.last limit 1;""" % (ip)
+            ORDER BY n.nb desc, n.last limit %s;""" , (ip, limit))
         cur.execute(query)
 
         if cur.rowcount == 0:
             # get one random building around our location
-            default_lat = '47.7927'
-            default_lon = '3.5840'
+            default_lat = '48.2169' # '47.7927'
+            default_lon = '-1.7521' # '3.5840'
             lat = float(req.params.get('lat',default_lat))
             lon = float(req.params.get('lon',default_lon))
             if (lat == float(default_lat)):
@@ -51,9 +59,8 @@ class BuildingsResource(object):
                 AND coalesce(n.total,0)<10 AND o1.ip IS NULL
                 GROUP by osm_id, geom, surface, b.orientation, n.nb, n.last
                 HAVING (count(o2.*)<10 or (count(distinct(o2.orientation))=1 AND count(o2.*)<=3))
-                ORDER BY %s LIMIT 1;""" % (ip,lon,lat,order)
+                ORDER BY %s LIMIT %s;""" % (ip,lon,lat,order,limit)
             cur.execute(query)
-        building = cur.fetchone()
 
         # cookie management
         cookies = req.cookies
@@ -65,10 +72,18 @@ class BuildingsResource(object):
         resp.set_header('X-Powered-By', 'OpenSolarMap')
         resp.set_header('Access-Control-Allow-Origin', '*')
         resp.set_header('Access-Control-Allow-Headers', 'X-Requested-With')
-        try:
-            resp.body = (building[0])
-        except:
-            print("no building")
+
+        if cur.rowcount > 1:
+            # return de Featurecollection when we have several buildings...
+            rows = cur.fetchall()
+            body = dict(count=cur.rowcount, type="Featurecollection", features=[json.loads(r[0]) for r in rows])
+            resp.body = json.dumps(body)
+        else:
+            try:
+                building = cur.fetchone()
+                resp.body = building[0]
+            except:
+                resp.body = "no building"
         db.close()
 
     def on_get(self, req, resp):
